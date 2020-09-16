183428388ac851941e74fc381db3e7d6c160e319
felipeweber <weberecomp@gmail.com>
2020-09-16 12:02:07 -0300

    created file extension UIView anchors

---
+++ Learn View Code/UIView+anchors.swift
@@ -0,0 +1,49 @@
+//
+//  UIView+anchors.swift
+//  Learn View Code
+//
+//  Created by Felipe Weber on 16/09/20.
+//  Copyright © 2020 Felipe Weber. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func fillSuperview(padding: UIEdgeInsets) {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor, padding: padding)
+    }
+    
+    func fillSuperview() {
+        fillSuperview(padding: .zero)
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailing.constraint(equalTo: trailing, constant: padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+}
